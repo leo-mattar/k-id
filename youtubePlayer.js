b142e2415e0737980a018abe3bd2cc4d8b2bd463
leo-mattar <126821704+leo-mattar@users.noreply.github.com>
2024-06-26 07:51:56 +0100

    Add files via upload

---
+++ youtubePlayer.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let youtubePlayer;
+
+  function onYouTubeIframeAPIReady() {
+    let videoId = "r_M_aJFwDPM";
+
+    if (window.location.pathname.startsWith("/ja")) {
+      videoId = "kcmd8NfaKak";
+      console.log("ja");
+    }
+
+    youtubePlayer = new YT.Player("youtube-player", {
+      width: "100%",
+      height: "100%",
+      videoId: videoId,
+      playerVars: { autoplay: 0, controls: 1, cc_load_policy: 1 },
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange,
+      },
+    });
+  }
+
+  function onPlayerReady(event) {
+    // console.log("YouTube player is ready");
+    // Optionally, you can also start playing the video here
+    // youtubePlayer.playVideo();
+  }
+
+  function onPlayerStateChange(event) {
+    // console.log("Player State Changed: ", event.data);
+    // You can handle different player states here if needed
+  }
+
+  function playVideo() {
+    if (youtubePlayer) {
+      youtubePlayer.playVideo();
+    }
+  }
+
+  function stopVideo() {
+    if (youtubePlayer) {
+      youtubePlayer.stopVideo();
+    }
+  }
+
+  document.querySelectorAll("[video-component]").forEach(function (videoCard) {
+    videoCard.addEventListener("click", function () {
+      document.querySelector(".c-modal").classList.add("is-open");
+      playVideo();
+      lenis.stop();
+    });
+
+    document
+      .querySelector(".c-modal-close")
+      .addEventListener("click", function () {
+        document.querySelector(".c-modal").classList.remove("is-open");
+        stopVideo();
+        lenis.start();
+      });
+  });
+});
